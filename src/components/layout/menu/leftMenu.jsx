490abf27c237f6fc8907ffcaa649fde1e84fdada
miaoyu <362334256@qq.com>
2017-06-20 00:07:46 +0800

    '提交基础业务模块'

--- src/components/layout/menu/leftMenu.jsx
+++ src/components/layout/menu/leftMenu.jsx
@@ -2,6 +2,18 @@ import './leftMenu.less';
 import React, { Component } from 'react';
 import ReactDOM, { render } from 'react-dom';
 
+// 当前组件全局命名
+const className = 'component-layout-menu-leftMenu';
+
+
+// logo组件
+const Logo = () => (
+    <h1 className={`${className}-logo text-center`}>
+        logo
+    </h1>
+);
+
+
 // 右侧抽屉导航
 class LeftMenu extends Component {
 	constructor(props) {
@@ -10,7 +22,8 @@ class LeftMenu extends Component {
 
 	render() {
 		return (
-			<nav className="component-layout-menu-leftMenu oya oxh">
+			<nav className={`${className} oya oxh`}>
+                <Logo />
 				<ul>
 					<li>导航导航导航导航导航导航导航导航导航导航导航导航导航导航导航导航导航导航导航导航导航导航导航导航导航导航导航导航</li>
 					<li>导航</li>
